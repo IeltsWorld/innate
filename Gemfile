a934453b97e41afffaa6b52580945566b30045e5
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2022-11-08 15:37:25 +0530

    feat: pg

--- Gemfile
+++ Gemfile
@@ -68,15 +68,12 @@ group :test do
     gem 'shoulda-matchers', '~> 5.0'
 end
 
-
-group :development do
-  # Use console on exceptions pages [https://github.com/rails/web-console]
-  gem "web-console"
-
-  # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
-  # gem "rack-mini-profiler"
-
-  # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
-  # gem "spring"
+group :production do 
+  gem "pg", "~> 1.1"
 end
 
+
+group :development do
+  gem "web-console"
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 
 
 user_avatars = []
 
-16.times do 
+16.times do =
     user_avatars << URI.parse(Faker::LoremFlickr.image).open
-end 
+end
 
 
 me = User.create(email: "vaibhav.upreti16@gmail.com", password: "idkidk", first_name: "meow", last_name: "meow")
