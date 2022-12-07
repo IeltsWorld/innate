4833fb89d04b6ec9c428ae3566f67f819c349f05
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2022-12-07 23:12:32 +0530

    feat: changes

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module IeltsApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.autoloader = :classic
 
   
     config.assets.initialize_on_precompile = false
